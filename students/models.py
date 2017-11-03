19446448a44855b2fd86b59a54b71f088c2852cf
somagh <aryakowsary@yahoo.com>
2017-11-03 21:31:31 +0330

    final

--- students/models.py
+++ students/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Student(models.Model):
     first_name = models.CharField(max_length=50,verbose_name='نام')
     last_name = models.CharField(max_length=50,verbose_name='نام خانوادگی')
-    birth_date = models.DateField(verbose_name='تاریخ تولد')
+    birth_date = models.DateField(verbose_name='تاریخ تولد',help_text='میلادی')
     national_code = models.CharField(max_length=10,verbose_name='شماره شناسنامه',unique=True,error_messages={'unique':'دانشجوی دیگری با این شماره شناسنامه در سامانه موجود است.'})
 
 
