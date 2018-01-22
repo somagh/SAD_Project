0664b9046df780b4936602a9e797c6c4e8fa8c33
somagh <aryakowsary@yahoo.com>
2018-01-22 16:54:27 +0330

    login added

--- SAD_Project/urls.py
+++ SAD_Project/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/', include('administrator.urls', namespace='admin')),
+    url(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
 ]
